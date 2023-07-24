54057e28b33e4d3102247249953529cc026187f2
narenkarn786 <narenkarn786@gmail.com>
2023-07-24 14:50:35 +0545

    Add project files.

---
+++ JWTWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace JWTWebAPI.Controllers
+{
+
+    
+    //[ApiController]
+    [Consumes("application/json")]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new User();
+
+        [HttpPost]
+        [Route("api/Auth/Register")]
+        public async Task<ActionResult<User>> Register([FromBody]UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.Username= request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Ok(user);
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+    }
+}
