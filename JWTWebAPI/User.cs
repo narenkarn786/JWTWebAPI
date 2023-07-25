1ad71eb420279ffda16c7884a551d0f049212824
narenkarn786 <narenkarn786@gmail.com>
2023-07-25 10:31:00 +0545

    Created JWT

--- JWTWebAPI/User.cs
+++ JWTWebAPI/User.cs
@@ -2,8 +2,8 @@
 {
     public class User
     {
-        public string? Username { get; set; }
-        public byte[]? PasswordHash { get; set; }
-        public byte[]? PasswordSalt { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
--- JWTWebAPI/UserDto.cs
+++ JWTWebAPI/UserDto.cs
@@ -2,7 +2,7 @@
 {
     public class UserDto
     {
-        public string? Username { get; set; }
-        public string? Password { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
