53d302eecbe5553f03aba3bfeb2e7ab0da451229
narenkarn786 <narenkarn786@gmail.com>
2023-07-28 12:57:26 +0545

    Added Refresh Token

--- JWTWebAPI/User.cs
+++ JWTWebAPI/User.cs
@@ -5,5 +5,9 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string RefreshToken { get; set; }=string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
